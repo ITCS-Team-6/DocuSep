b8eb59b9bc968525a84b1db019cd2304f73c95a1
Cruxk <brpusso@gmail.com>
2022-10-19 13:38:02 -0400

    Initial commit

---
+++ Test.py
@@ -0,0 +1,76 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from extra_keras_datasets import emnist
+from keras.utils import np_utils
+from keras.datasets import mnist
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+data = r"C:\Users\Brenden\PycharmProjects\pythonProject\Machine Printed\Handwritten\A_Z Handwritten Data.csv"
+
+dataset = pd.read_csv(data).astype("float32")
+dataset.rename(columns={'0': "label"}, inplace=True)
+
+letter_x = dataset.drop("label", axis = 1)
+letter_y = dataset["label"]
+
+letter_x = letter_x.values
+
+print(letter_x.shape, letter_y.shape)
+
+data = []
+
+for flatten in letter_x:
+    image = np.reshape(flatten, (28,28,1))
+    data.append(image)
+
+letter_data = np.array(data, dtype=np.float32)
+letter_target = letter_y
+
+print(letter_data.shape, letter_target.shape)
+
+shuffle_data = shuffle(letter_data)
+rows,cols = 10 ,10
+
+plt.figure(figsize=(20,20))
+
+for i in range (rows * cols):
+    plt.subplot(cols, rows, i+1)
+    plt.imshow(shuffle_data[i].reshape(28,28), interpolation="nearest", cmap="gray")
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
