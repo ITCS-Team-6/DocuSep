ef649603edf39bef9039e76d9b1a415baaf9d62a
Cruxk <115195717+Cruxk@users.noreply.github.com>
2022-10-20 18:33:24 -0400

    added link for digital letter datset

--- DocuSep.py
+++ DocuSep.py
@@ -8,6 +8,7 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
 #link for dataset: https://www.kaggle.com/datasets/sachinpatel21/az-handwritten-alphabets-in-csv-format
+#Digital: https://www.kaggle.com/datasets/adamkaniasty/digital-letters
 #pathing will determine where your files are located
 data = r"Datasets/Handwritten/A_Z Handwritten Data.csv"
 data_2 = r"Datasets/Machine/digital_letters.csv"
