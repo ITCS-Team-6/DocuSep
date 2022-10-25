df5620c510c01a3b05b577683c803d2bc6338b38
Cruxk <115195717+Cruxk@users.noreply.github.com>
2022-10-25 18:05:20 -0400

    Update DocuSep_training.py

--- DocuSep_training.py
+++ DocuSep_training.py
@@ -8,9 +8,17 @@ train_labels = np.load(r"numpy/train_labels.npy")
 test_data = np.load(r"numpy/test_data.npy")
 test_labels = np.load(r"numpy/test_labels.npy")
 
+#these are parameters that go into these parenthesis except sequential, testing to figure out what numbers and such work
+#also these are layers added to train the data the dataset hasnt been applied
+#may need more of each testing
 model = Sequential()
 
+model.add(Conv2D()
+model.add(BatchNormalization())
+model.add(MaxPool2D()
 
+model.add(Flatten())
+model.add(Dense())
 
 
 
