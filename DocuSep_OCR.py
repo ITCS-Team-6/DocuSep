1844050df0b48e0090ef2dc913f80dd34fc938ba
Cruxk <115195717+Cruxk@users.noreply.github.com>
2022-11-02 16:25:31 -0400

    Update DocuSep_OCR.py

--- DocuSep_OCR.py
+++ DocuSep_OCR.py
@@ -25,32 +25,32 @@ cropper()
 
 imgs = []
 for img in glob.glob("croppedimg/*.png"):
-    n = cv2.imread(img)
-    imgs.append(n)
+    loop_img= cv2.imread(img)
+    imgs.append(loop_img)
 
 
-def get_grayscale(n):
-    return cv2.cvtColor(n, cv2.COLOR_BGR2GRAY)
-gray = get_grayscale(n)
+def get_grayscale(loop_img):
+    return cv2.cvtColor(loop_img, cv2.COLOR_BGR2GRAY)
+gray = get_grayscale(loop_img)
 Image.fromarray(gray)
 
-def thresholding(n):
-    return cv2.threshold(n, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+def thresholding(loop_image):
+    return cv2.threshold(loop_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 thresh = thresholding(gray)
 Image.fromarray(thresh)
 
 imgs = []
 for img in glob.glob("croppedimg/*.png"):
-    n = cv2.imread(img)
-    imgs.append(n)
-    n = get_grayscale(n)
-    n = thresholding(n)
-    h, w = n.shape
-    boxes = pytesseract.image_to_boxes(n)
+    loop_img = cv2.imread(img)
+    imgs.append(loop_img)
+    loop_img = get_grayscale(loop_img)
+    loop_img= thresholding(loop_img)
+    h, w = loop_img.shape
+    boxes = pytesseract.image_to_boxes(loop_img)
     for b in boxes.splitlines():
         b = b.split(' ')
-        cv2.rectangle(n, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+        cv2.rectangle(loop_img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
     cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("Result", 1280, 768)
-    cv2.imshow('Result', n)
+    cv2.imshow('Result', loop_img)
     cv2.waitKey()
