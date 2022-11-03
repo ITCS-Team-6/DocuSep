e4a4b87fae220ce5b834e1bf4770f08fe7b3ed09
Cruxk <brpusso@gmail.com>
2022-11-02 20:53:57 -0400

    uploaded imgs, implementation and empty cropped img folder

--- DocuSep_OCR.py
+++ DocuSep_OCR.py
@@ -31,13 +31,15 @@ for img in glob.glob("croppedimg/*.png"):
 
 def get_grayscale(loop_img):
     return cv2.cvtColor(loop_img, cv2.COLOR_BGR2GRAY)
-gray = get_grayscale(loop_img)
-Image.fromarray(gray)
 
-def thresholding(loop_image):
-    return cv2.threshold(loop_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-thresh = thresholding(gray)
-Image.fromarray(thresh)
+
+def thresholding(loop_img):
+    return cv2.threshold(loop_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
+def remove_noise(loop_img):
+    return cv2.medianBlur(loop_img,5)
+
 # test
 imgs = []
 for img in glob.glob("croppedimg/*.png"):
@@ -45,13 +47,17 @@ for img in glob.glob("croppedimg/*.png"):
     imgs.append(loop_img)
     loop_img = get_grayscale(loop_img)
     loop_img= thresholding(loop_img)
+    loop_img = remove_noise(loop_img)
     h, w = loop_img.shape
     boxes = pytesseract.image_to_boxes(loop_img)
+    bw = []
+    bh = []
     for b in boxes.splitlines():
         b = b.split(' ')
         cv2.rectangle(loop_img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
     cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("Result", 1280, 768)
     cv2.imshow('Result', loop_img)
+    print(len(bw))
+    print(len(bh))
     cv2.waitKey()
-    print(boxes)
