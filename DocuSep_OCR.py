c7ba3dd75f57b9f7c62ef08a17377ce6ce423ce6
Cruxk <brpusso@gmail.com>
2022-12-02 15:18:34 -0500

    added gui, pdf and user input

--- DocuSep_OCR.py
+++ DocuSep_OCR.py
@@ -1,129 +1,150 @@
-from Functions import *
-import random
-import cv2
-import numpy as np
-import pytesseract
-import glob
-import matplotlib.pyplot as plt
+import tkinter
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+from test import *
 
 pytesseract.pytesseract.tesseract_cmd =r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-cropper()
-imgs = []
-# we use glob to find files within folder that have png extension
 
-imglist = glob.glob("croppedimg/*")
-random.shuffle(imglist)
-for img in imglist:
-    loop_img = cv2.imread(img)
-    imgs.append(loop_img)
-    # truncate function
-    loop_img = get_grayscale(loop_img)
-    loop_img = thresholding(loop_img)
-    # loop_img = remove_noise(loop_img)
+root = Tk()
+root.title('DocuSep')
 
-    h, w = loop_img.shape
-    boxes = pytesseract.image_to_boxes(loop_img)
-    bw = []
-    bh = []
+l1 = tkinter.Label(root, text= 'DocuSep', width = 30)
+l1.grid(row=1, column=1, columnspan=4)
+b1 = tkinter.Button(root, text='Upload Documents',width=20,command = lambda:upload_file())
+b1.grid(row = 2, column=1, columnspan=4)
 
-    blkpix = []
-    max_hor = []
-
-    # this is for getting all the black pixels on the image for max horizontal run
-    for b in boxes.splitlines():
-        b = b.split(' ')
-        test_img = loop_img[h - int(b[4]): h - int(b[2]), int(b[1]):int(b[3])]
-        th, tw = test_img.shape
-        max = 0
-        for i in range(th):
-            count = 0
-            for j in range(tw):
-                if test_img[i, j] == 0:
-                    count += 1
-                else:
-                    if max < count:
-                        max = count
-                    count = 0
-            if max < count:
-                max = count
-            count = 0
-
-        num_black_pix = np.sum(test_img == 0)
-        blkpix.append(num_black_pix)
-        max_hor.append(max)
+def upload_file():
+    file_types = [('Image Files', '*.jpg *.png *.jpeg *.pdf')]
+    filename = tkinter.filedialog.askopenfilenames(filetypes=file_types)
+    col=1
+    row=3
 
 
-    for b in boxes.splitlines():
-        b = b.split(' ')
+    for f in filename:
+        loop_img = cropper(f)
+        loop_img = get_grayscale(loop_img)
+        loop_img = thresholding(loop_img)
+        # loop_img = remove_noise(loop_img)
 
-        cv2.rectangle(loop_img, (int(b[1]), h-int(b[2])), (int(b[3]), h-int(b[4])), (0, 255, 0), 2)
+        h, w = loop_img.shape
+        boxes = pytesseract.image_to_boxes(loop_img)
+        bw = []
+        bh = []
 
-        # Adding the calculated coordinated to their arrays
-        bw.append(int(b[3]) - int(b[1]))
-        bh.append(int(b[4]) - int(b[2]))
-    cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
-    # cv2.resizeWindow("Result", 1280, 768)
-    cv2.imshow('Result', loop_img)
+        blkpix = []
+        max_hor = []
 
-    # Prints the amount of boudning boxes
-    sums = np.array(blkpix)
-    sus_max_hor = np.array(max_hor)
-    new_bw = np.array(bw)
-    # print(len(bw))
+        # this is for getting all the black pixels on the image for max horizontal run
+        for b in boxes.splitlines():
+            b = b.split(' ')
+            test_img = loop_img[h - int(b[4]): h - int(b[2]), int(b[1]):int(b[3])]
+            th, tw = test_img.shape
+            max = 0
+            for i in range(th):
+                count = 0
+                for j in range(tw):
+                    if test_img[i, j] == 0:
+                        count += 1
+                    else:
+                        if max < count:
+                            max = count
+                        count = 0
+                if max < count:
+                    max = count
+                count = 0
 
-    # Removing sums less than 50 and greater than 5000
-    new_sums = np.delete(sums, np.where(sums < 51))
-    new_sums = np.delete(new_sums, np.where(new_sums > 5001))
-    # print(new_sums)
+            num_black_pix = np.sum(test_img == 0)
+            blkpix.append(num_black_pix)
+            max_hor.append(max)
 
-    # Getting duplicate sums and how many there are
-    dupe, counts = np.unique(new_sums, return_counts=True)
-    dupe = dupe[counts > 1]
-    counts = counts[counts > 1]
+        for b in boxes.splitlines():
+            b = b.split(' ')
 
-    new_max_hor = np.delete(sus_max_hor, np.where(sus_max_hor < 11))
+            cv2.rectangle(loop_img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
 
-    new_bw = np.delete(new_bw, np.where(sus_max_hor < 11))
+            # Adding the calculated coordinated to their arrays
+            bw.append(int(b[3]) - int(b[1]))
+            bh.append(int(b[4]) - int(b[2]))
 
-    ratio = np.divide(new_max_hor, new_bw)
-
-    avg_ratio = np.average(ratio)
-
-    counts_sums = np.sum(counts)
-
-    match_scr = counts_sums / len(new_sums)
-
-    # print(dupe)
-    # print(counts)
+        # cv2.resizeWindow("Result", 1280, 768)
 
 
-    # Histogram
-    plt.hist(new_sums, bins=100, edgecolor="red")
-    plt.xlabel('Sums')
-    plt.ylabel('Num of Occurrences')
-    plt.show()
+        # Prints the amount of boudning boxes
+        sums = np.array(blkpix)
+        sus_max_hor = np.array(max_hor)
+        new_bw = np.array(bw)
+        # print(len(bw))
 
-    # Conditionals
+        # Removing sums less than 50 and greater than 5000
+        new_sums = np.delete(sums, np.where(sums < 51))
+        new_sums = np.delete(new_sums, np.where(new_sums > 5001))
+        # print(new_sums)
 
-    if len(bw) <= 30:
-        print("This is Unknown")
-    elif match_scr > .5:
-        print("This is Machine Printed")
-    elif avg_ratio > .8:
-        print("This is Machine Printed")
-    elif avg_ratio < .5:
-        print("This is Handwritten")
-    elif avg_ratio > .7 and match_scr > .05:
-        print("This is Machine Printed")
-    elif match_scr < .3:
-        print("This is Handwritten")
-    elif match_scr > .4:
-        print("This is Machine Printed")
-    else:
-        print("This is Unknown")
+        # Getting duplicate sums and how many there are
+        dupe, counts = np.unique(new_sums, return_counts=True)
+        dupe = dupe[counts > 1]
+        counts = counts[counts > 1]
 
-cv2.waitKey(0)
+        new_max_hor = np.delete(sus_max_hor, np.where(sus_max_hor < 11))
+
+        new_bw = np.delete(new_bw, np.where(sus_max_hor < 11))
+
+        ratio = np.divide(new_max_hor, new_bw)
+
+        avg_ratio = np.average(ratio)
+
+        counts_sums = np.sum(counts)
+
+        match_scr = counts_sums / len(new_sums)
+
+        # print(dupe)
+        # print(counts)
 
 
 
 
+        #handling of image
+        img = Image.open(f)
+        img = ImageTk.PhotoImage(img)
+
+        # Conditionals
+
+        if len(bw) <= 30:
+            print("This is Unknown")
+        elif match_scr > .5:
+            print("This is Machine Printed")
+        elif avg_ratio > .8:
+            print("This is Machine Printed")
+        elif avg_ratio < .5:
+            print("This is Handwritten")
+        elif avg_ratio > .7 and match_scr > .05:
+            print("This is Machine Printed")
+        elif match_scr < .3:
+            print("This is Handwritten")
+        elif match_scr > .4:
+            print("This is Machine Printed")
+        else:
+            print("This is Unknown")
+
+
+
+        e1 = tkinter.Label(root)
+        e1.grid(row=row, column=col)
+        e1.image = img
+        e1['image'] = img
+        if (col == 3):
+            row = row + 1
+            col = 1
+        else:
+            col = col + 1
+
+
+
+
+
+
+
+
+
+root.mainloop()
--- Functions.py
+++ Functions.py
@@ -1,31 +1,32 @@
+
 import cv2
-import os
 import numpy as np
+import pytesseract
 from pdf2image import convert_from_path
+
+pytesseract.pytesseract.tesseract_cmd =r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 dir_path= 'images'
 
-def cropper():
-    files = [f for f in os.listdir(dir_path)]
-    for file in files:
-        filename = dir_path + '/' + file
-        image = None
-        if filename.endswith('.pdf'):
-            pages = convert_from_path(filename,
-                                      poppler_path=r'C:\Program Files\poppler-0.68.0\bin')
-            image = np.array(pages[0])
-            image = cv2.resize(image, None, fx=0.5, fy=0.5)
+def cropper(filename):
+    image = None
+    if filename.endswith('.pdf'):
+        pages = convert_from_path(filename,
+                                  poppler_path=r'C:\Program Files\poppler-0.68.0\bin')
+        image = np.array(pages[0])
+        image = cv2.resize(image, None, fx=0.5, fy=0.5)
 
-        elif filename.endswith(('.jpg', '.jpeg', '.png')):
-            image = cv2.imread(filename)
-        h,w,c = image.shape
-        cropped_img = image
-        if h > 2800:
-            cropped_img = image[750:2800, :]
-        if filename.endswith('.pdf'):
-            cv2.imwrite('croppedimg/' + file + '.png', cropped_img)
-        else:
-            cv2.imwrite('croppedimg/' + file, cropped_img)
+    elif filename.endswith(('.jpg', '.jpeg', '.png')):
+        image = cv2.imread(filename)
+    h,w,c = image.shape
+    cropped_img = image
+    if h > 1500:
+        cropped_img = image[750:1500, :]
+    # if filename.endswith('.pdf'):
+    return cropped_img
+    #     # cv2.imwrite('croppedimg/' + filename + '.png', cropped_img)
+    # else:
+    #     # cv2.imwrite('croppedimg/' + filename, cropped_img)
 
 def get_grayscale(loop_img):
     return cv2.cvtColor(loop_img, cv2.COLOR_BGR2GRAY)
